1a591fcc62fb885c504786aab2ce23c43d5fbe37
Humphrey <humphrey.asante@gmail.com>
2016-07-28 13:05:41 +0300

    Removed unused modules dependencies from core feature and menu function from template file

--- sites/all/themes/tree/template.php
+++ sites/all/themes/tree/template.php
@@ -247,16 +247,6 @@ function tree_preprocess_html(&$vars){
 
 
 
-function tree_links__topbar_main_menu($variables) {
-  // We need to fetch the links ourselves because we need the entire tree.
-  $links = menu_tree_output(menu_tree_all_data(variable_get('menu_main_links_source', 'main-menu')));
-  $output = _zurb_foundation_links($links);
-  $variables['attributes']['class'][] = 'right';
-
-
-
-  return '<ul class="right"' . drupal_attributes($variables['attributes']) . '>' . $output . '</ul>';
-}
 
 
 /**
